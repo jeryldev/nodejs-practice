ae161593fd5e99ba9eab34d2000eb79792047fde
JerylDEv <jeryldev@gmail.com>
2020-06-25 19:23:42 +0800

    moved routes to previous_lessons folder

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
-const routes = require('./routes');
+// const routes = require('./routes'); Introduction to node js
 
 console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
+const server = http.createServer();
 
 server.listen(4000);
