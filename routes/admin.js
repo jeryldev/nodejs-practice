dec734cf049ef698e3c2e6eb0c2f79ae45aa82cf
JerylDEv <jeryldev@gmail.com>
2020-06-26 07:55:18 +0800

    created 2 views, edited the admin route

--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,11 @@ router.get('/add-product', (req, res, next) => {
   res
     .status(200)
     .send(
-      '<form action="/product" method="post"><input type="text" name="productname"/><button type="submit">Add Product</button></form>'
+      '<form action="/admin/add-product" method="post"><input type="text" name="productname"/><button type="submit">Add Product</button></form>'
     );
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.send(`<h1>${req.body.productname} was added!</h1>`);
   // res.redirect('/');
