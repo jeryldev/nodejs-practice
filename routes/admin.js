bdf5b2aef8aa55d2e91f77fb8bc8338bbece0e50
JerylDEv <jeryldev@gmail.com>
2020-06-28 07:15:11 +0800

    created shop.pug and add-product.pug

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,19 @@ const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-  res.status(200).sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // res.status(200).sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product');
 });
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
+  // console.log('products::', products);
   res.redirect('/');
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,14 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  // console.log(req.body.title);
-  res.status(200).sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // res.status(200).sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const productList = adminData.products;
+  res.render('shop', { products: productList, docTitle: 'Shop' });
 });
 
 module.exports = router;
